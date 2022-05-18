5450789654176f6150fea3ec8587b61db5231c99
billyzheng <billyzheng.bz@gmail.com>
2022-05-18 12:14:59 -0400

    added wait time, fixed path

--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
 import pyglet
 from f1tenth_player import F1TenthVideoPlayer
 import os
-
+import time
 SRC_PATH = os.path.dirname(__file__)
 PROJECT_PATH = os.path.join(SRC_PATH, '..')
 MAP_PATH = os.path.join(PROJECT_PATH, 'resources', 'maps', 'Oschersleben')
 
 if __name__ == "__main__":
     # sample_log = os.path.join(PROJECT_PATH, 'resources', 'examples', 'logs', 'multi.jsonl')
-    sample_log = "/Users/johannesbetz/Desktop/f1tenth-log-player/logs/3rdPlace/Race1_round-2.jsonl"
+    sample_log = "./logs/Round1/match-343588-round-1.jsonl"
 
     player = F1TenthVideoPlayer(sample_log, MAP_PATH, 2000, 2000)
     pyglet.clock.schedule_interval(player.update, 0.010)
+    time.sleep(0.5)
     pyglet.app.run()
